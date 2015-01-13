ea9d6583a4ad88d9481b1cb73c1f0601895f3338
Shaba Abhiram <shabarivas@gmail.com>
2015-01-13 12:22:22 -0800

    Moar tests

--- chloe_test.go
+++ chloe_test.go
@@ -2,16 +2,35 @@
 package main
 
 import (
+    "os"
+    "fmt"
     "testing"
     "strings"
+
+    "path/filepath"
+    "io/ioutil"
 
     "github.com/stretchr/testify/assert"
 )
 
-// Test application usage string printing
-func TestChloeUsage(test *testing.T) {
+const (
+    TEST_DIR = "tmp_test_dir"
+)
 
+// Helper function to setup a test fixture dir and write to
+// it a file with the name "fname" and content "content"
+func writeFileToTestDir(fname, content string) {
+    testDirPath := "." + string(filepath.Separator) + TEST_DIR
+    testFilePath := testDirPath + string(filepath.Separator) + fname
+
+    _ = os.MkdirAll(testDirPath, 0755)
+    _ = ioutil.WriteFile(testFilePath, []byte(content), os.ModePerm)
 }
+
+func cleanupTestDir() {
+    _ = os.RemoveAll(fmt.Sprintf(".%s%s", string(filepath.Separator), TEST_DIR))
+}
+
 
 // Test Helper functions
 func TestContainsString(test *testing.T) {
@@ -40,4 +59,34 @@ func TestIsValidCommand(test *testing.T) {
         assert.Equal(test, true, isValidCommand(item.name), "valid commands should be valid")
     }
     assert.Equal(test, false, isValidCommand("invalid_command"), "invalid commands should be rejected")
+}
+
+func TestGetAppUsage(test *testing.T) {
+    usageStr := getAppUsageString()
+    assert.Contains(test, usageStr, "<command> [<options>]", "usage must contain sample usage str")
+}
+
+func TestGetAppVersion(test *testing.T) {
+    version := getAppVersionString()
+    assert.Contains(test, version, Version, "version must match internal global var")
+}
+
+func TestGetIgnoreObjectFromFile(test *testing.T) {
+    writeFileToTestDir("test.json", `
+{
+    "chloe": [
+        "node_modules",
+        "**/*.out"
+    ]
+}
+`)
+    defer cleanupTestDir()
+
+    object, err := getIgnoreObjectFromJSONFile("tmp_test_dir/test.json")
+    assert.Nil(test, err, "err should be nil")
+
+    assert.Equal(test, false, object.MatchesPath("foo"), "foo should not match")
+
+    assert.Equal(test, true,  object.MatchesPath("foobar/blah.out"),  "foobar/blah.out should match")
+    assert.Equal(test, true,  object.MatchesPath("node_modules/foo"), "node_modules/foo should match")
 }
